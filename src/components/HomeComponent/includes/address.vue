1ee353dfff3fbf2a91a4e45d08c6102578f199e5
Leandro Bezerra <suporte@internetlojas.com>
2019-06-04 17:07:52 -0300

    Corrigido slider e google maps

--- src/components/HomeComponent/includes/address.vue
+++ src/components/HomeComponent/includes/address.vue
@@ -1,122 +1,129 @@
 <template>
 <div>
-  <div class="il-home--address">
-    <p class="il-weight il-color--text__dark"><i class="mdi mdi-map mdi-12px"></i>{{kayzenAddress}}, {{kayzenCity}}</p>    
-    <p class="il-weight il-color--text__dark"><i class="mdi mdi-phone mdi-12px"></i>{{phone.ddd}} {{phone.number}}</p>
-    <p class="il-weight il-color--text__dark"><i class="mdi mdi-cellphone-iphone mdi-12px"></i>{{mobil.ddd}} {{mobil.number}}</p>
-    <p class="il-weight il-color--text__dark"><i class="mdi mdi-earth mdi-12px"></i>{{kayzenCity}}</p>
-    <p class="il-weight il-color--text__dark"><i class="mdi mdi-email-variant mdi-12px"></i>{{kayzenEmail}}</p>
-    <p class="il-weight il-color--text__dark"><i class="mdi mdi-map-marker-radius mdi-12px" ></i><a href="#!" class="il-color--text__red" @click.prevent="loadMap()" :class="isDisabled" >veja o mapa</a></p>
-    <div class="il-clock" v-show="showLoad"></div>
-</div>
-<ilMap v-if="checkShowMap" :show="showMap" />
+    <div class="il-home--address">
+        <p class="il-weight il-color--text__dark"><i class="mdi mdi-map mdi-12px"></i>{{kayzenAddress}}, {{kayzenCity}}</p>
+        <p class="il-weight il-color--text__dark"><i class="mdi mdi-phone mdi-12px"></i>{{phone.ddd}} {{phone.number}}</p>
+        <p class="il-weight il-color--text__dark"><i class="mdi mdi-cellphone-iphone mdi-12px"></i>{{mobil.ddd}} {{mobil.number}}</p>
+        <p class="il-weight il-color--text__dark"><i class="mdi mdi-earth mdi-12px"></i>{{kayzenCity}}</p>
+        <p class="il-weight il-color--text__dark"><i class="mdi mdi-email-variant mdi-12px"></i>{{kayzenEmail}}</p>
+        <p class="il-weight il-color--text__dark"><i class="mdi mdi-map-marker-radius mdi-12px" ></i><a href="#!" class="il-color--text__red" @click.prevent="loadMap()" :class="isDisabled" >veja o mapa</a></p>
+        <div class="il-clock" v-show="showLoad"></div>
+    </div>
+    <ilMap v-if="checkShowMap" :show="showMap" />
 </div>
 </template>
 
 <script>
 import ilMap from './map.vue';
 export default {
-  name: 'localization',
-  components: {
-    ilMap
-  },
-  mounted() {
-    this.createScript();
-  },
-  data() {
-    return {
-      kayzenAddress:
-        'Av Lavras 334, Bairro Petrópolis, Sala 702, CEP - 90460-040',
-      kayzenCity: 'Porto Alegre - RS',
-      kayzenEmail: 'contato@kaizenpilates.com',
-      phone: {
-        ddd: '51',
-        number: '3519-3339'
-      },
-      mobil: {
-        ddd: '51',
-        number: '99301-5930'
-      },
-      showMap: false,
-      showLoad: false
-    };
-  },
-  computed: {
-    checkShowMap() {
-      if (this.showMap) {
-        return true;
-      }
-      return false;
+    name: 'localization',
+    components: {
+        ilMap
     },
-    isDisabled() {
-      if (this.showMap) {
-        return 'isDisabled';
-      } else {
-        return '';
-      }
-    }
-  },
-  methods: {
-    removeScript(filename) {
-      var tags = document.getElementsByTagName('script');
-      for (var i = tags.length; i >= 0; i--) {
-        if (
-          tags[i] &&
-          tags[i].getAttribute('src') != null &&
-          tags[i].getAttribute('src').indexOf(filename) != -1
-        )
-          tags[i].parentNode.removeChild(tags[i]);
-      }
+    mounted() {
+        this.createScript();
     },
-    createScript() {
-      this.showMap = false;
-      const key = 'AIzaSyBJ9usSMvev68rfDKlnHPsJbsRoSL2tZqQ';
-      require('@google/maps').createClient({
-        key: key
-      });
-
-      let googleMapScript = document.createElement('SCRIPT');
-      if (typeof window === 'undefined') {
-        // Do nothing if run from server-side
-        return Promise.resolve();
-      }
-      if (!window.google || !window.google.map) {
-        var options = {};
-        if (typeof key === 'string') {
-          options.key = key;
-        } else {
-          if (typeof key === 'object') {
-            for (let k in key) {
-              // transfer values in apiKey to options
-              options[k] = key[k];
+    data() {
+        return {
+            kayzenAddress: 'Av Lavras 334, Bairro Petrópolis, Sala 702, CEP - 90460-040',
+            kayzenCity: 'Porto Alegre - RS',
+            kayzenEmail: 'contato@kaizenpilates.com',
+            phone: {
+                ddd: '51',
+                number: '3519-3339'
+            },
+            mobil: {
+                ddd: '51',
+                number: '99301-5930'
+            },
+            showMap: false,
+            showLoad: false
+        };
+    },
+    computed: {
+        checkShowMap() {
+            if (this.showMap) {
+                return true;
             }
-          }
+            return false;
+        },
+        isDisabled() {
+            if (this.showMap) {
+                return 'isDisabled';
+            } else {
+                return '';
+            }
         }
-      } else {
-        throw new Error('`key` should either be a string or an object');
-      }
-
-      const baseUrl = 'https://maps.googleapis.com';
-      const urlParams = Object.keys(options)
-        .map(
-          key =>
-            `${encodeURIComponent(key)}=${encodeURIComponent(options[key])}`
-        )
-        .join('&');
-      let url = `${baseUrl}/maps/api/js?${urlParams}`;
-      this.removeScript(url);
-      googleMapScript.setAttribute('src', url);
-      googleMapScript.setAttribute('async', '');
-      googleMapScript.setAttribute('defer', '');
-      document.body.appendChild(googleMapScript);
     },
-    loadMap() {
-      this.showLoad = true;
-      setTimeout(() => {
-        this.showLoad = false;
-        this.showMap = true;
-      }, 3000);
+    methods: {
+        removeScript(filename) {
+            let check = false
+            var tags = document.getElementsByTagName('script');
+            for (var i = tags.length; i >= 0; i--) {
+
+                if (
+                    tags[i] &&
+                    tags[i].getAttribute('src') != null &&
+                    tags[i].getAttribute('src').indexOf(filename) != -1
+                ) {
+                    check = true
+                    tags[i].parentNode.removeChild(tags[i])
+                };
+            }
+            return check
+        },
+        createScript() {
+            this.showMap = false;
+            const key = 'AIzaSyBJ9usSMvev68rfDKlnHPsJbsRoSL2tZqQ';
+            require('@google/maps').createClient({
+                key: key
+            });
+
+            let googleMapScript = document.createElement('SCRIPT');
+            if (typeof window === 'undefined') {
+                // Do nothing if run from server-side
+                return Promise.resolve();
+            }
+
+            if (!window.google || !window.google.map) {
+
+                var options = {};
+                if (typeof key === 'string') {
+                    options.key = key;
+                } else {
+                    if (typeof key === 'object') {
+                        for (let k in key) {
+                            // transfer values in apiKey to options
+                            options[k] = key[k];
+                        }
+                    }
+                }
+            } else {
+                throw new Error('`key` should either be a string or an object');
+            }
+
+            const baseUrl = 'https://maps.googleapis.com';
+            const urlParams = Object.keys(options)
+                .map(
+                    key =>
+                    `${encodeURIComponent(key)}=${encodeURIComponent(options[key])}`
+                )
+                .join('&');
+            let url = `${baseUrl}/maps/api/js?${urlParams}`;
+            if (!this.removeScript(url)) {
+                googleMapScript.setAttribute('src', url);
+                googleMapScript.setAttribute('async', '');
+                googleMapScript.setAttribute('defer', '');
+                document.body.appendChild(googleMapScript);
+            }
+        },
+        loadMap() {
+            this.showLoad = true;
+            setTimeout(() => {
+                this.showLoad = false;
+                this.showMap = true;
+            }, 3000);
+        }
     }
-  }
 };
 </script>
--- src/components/HomeComponent/includes/popup.vue
+++ src/components/HomeComponent/includes/popup.vue
@@ -1,16 +1,16 @@
 <template>
-    <!-- popup -->
-    <div class="popScroll" :class="getCurrentClass()">
-        <div class="popup">
-            <span class="ribbon top-left ribbon-primary">
+<!-- popup -->
+<div class="popScroll" :class="getPopClass()">
+    <div class="popup">
+        <span class="ribbon top-left ribbon-primary">
                 <small>KAIZEN</small>
             </span>
-            <h1>Seja Bem Vindo!</h1>
-            <p>Agende uma aula experimental grátis!</p>
-            <div id="option">
-                
-                <div class='il-picture'>
-                  <img id="slider" src="/images/gallery/studio/kaizen1.jpg" alt="kaizen1.jpg" title="Espaço Kaizen">
+        <h1>Seja Bem Vindo!</h1>
+        <p>Agende uma aula experimental grátis!</p>
+        <div id="option">
+
+            <div class='il-picture'>
+                <img id="slider" src="/images/gallery/studio/kaizen1.jpg" alt="kaizen1.jpg" title="Espaço Kaizen">
                 </div>
 
                 <router-link id="agendar" class="boxi" :to="{name:'contact', params: {plan:'message'}}" title="Agendar uma aula experimental">Agendar</router-link>
@@ -23,51 +23,59 @@
 
 <script>
 export default {
-  name: 'popup',
-  data() {
-    return {
-      slider: [
-        'kaizen1',
-        'kaizen2',
-        'kaizen3',
-        'kaizen4',
-        'kaizen5',
-        'kaizen6',
-        'kaizen7',
-        'kaizen8'
-      ],
-      closed: false
-    };
-  },
-  mounted() {
-    this.rotateSlider();
-  },
-  methods: {
-    closePop() {
-      this.closed = true;
+    name: 'popup',
+    data() {
+        return {
+            slider: [
+                'kaizen1',
+                'kaizen2',
+                'kaizen3',
+                'kaizen4',
+                'kaizen5',
+                'kaizen6',
+                'kaizen7',
+                'kaizen8'
+            ],
+            closed: false,
+            interval: null
+        };
     },
-    getCurrentClass() {
-      if (!this.closed) {
-        return 'show';
-      } else {
-        return 'hidden';
-      }
+    mounted() {
+        this.rotateSlider();
     },
-    rotateSlider() {
-      const sliders = document.getElementById('slider');
-      let init = 0;
-      let end = 7;
-      setInterval(() => {
-        let current = init;
-        sliders.src = '/images/gallery/studio/' + this.slider[current] + '.jpg';
-
-        if (init <= end) {
-          init++;
-        } else {
-          init = 0;
+    methods: {
+        closePop() {
+            this.closed = true;
+            this.$parent.showPopup = false
+            clearInterval(this.interval)
+            this.interval = null
+        },
+        getPopClass() {
+            if (!this.closed) {
+                return 'show';
+            } else {
+                this.rotateSlider();
+                return 'hidden';
+            }
+        },
+        rotateSlider() {
+            const sliders = document.getElementById('slider');
+            sliders.src = '/images/gallery/studio/kaizen1.jpg';
+            let init = 0;
+            let end = 7;
+            if (!this.closed) {
+                this.interval = setInterval(() => {
+                    let current = init;
+                    if (init < end) {
+                        sliders.src = '/images/gallery/studio/' + this.slider[current] + '.jpg';
+                        init++;
+                    } else {
+                        sliders.src = '/images/gallery/studio/' + this.slider[current] + '.jpg';
+                        init = 0;
+                    }
+                }, 4000);
+            }
         }
-      }, 4000);
     }
-  }
 };
 </script>
